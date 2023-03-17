7c12dd45d0a11a1fdb6330e32dad3567572a0ad3
sashasyrovatko <oleksandrasyrovatko@gmail.com>
2023-03-17 17:01:23 +0300

    make some changes PEP8

--- day 3/Love.py
+++ day 3/Love.py
@@ -7,7 +7,7 @@ t = lower_case_string.count("t")
 r = lower_case_string.count("r")
 u = lower_case_string.count("u")
 e = lower_case_string.count("e")
-true = t + r + u +e
+true = t + r + u + e
 l = lower_case_string.count("l")
 o = lower_case_string.count("o")
 v = lower_case_string.count("v")
@@ -16,7 +16,7 @@ love = l + o + v + e
 love_score = int(str(true) + str(love))
 if (love_score < 10) or (love_score > 90):
     print(f"Your score is {love_score},you go together like coke and mentos")
-elif(love_score >= 40) and (love_score <=50):
+elif(love_score >= 40) and (love_score <= 50):
     print(f"Your score is {love_score}, you are alright together.")
 else:
     print(f"Your score is {love_score}")
