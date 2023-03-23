5272b6d687e3528b31f7f61f37b17d5d612769eb
sashasyrovatko <oleksandrasyrovatko@gmail.com>
2023-03-23 17:07:57 +0300

    (OOP) Quiz game

---
+++ Day 17/Day 17.py
@@ -0,0 +1,27 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+user_1 = User("001", "sasha")
+# user_1.id = "001"
+# user_1.username = "Sasha"
+
+print(user_1.username)
+
+user_2 = User("002", "dima")
+# user_2.id = "002"
+# user_2.username = "dima"
+print(user_2.id)
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
